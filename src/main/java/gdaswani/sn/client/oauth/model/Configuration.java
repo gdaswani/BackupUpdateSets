2823d7942cb4f74c608f0cb7f41f1e1c76a46515
gdaswani <georgedaswani@gmail.com>
2024-12-18 21:47:41 -0800

    Add files via upload

--- src/main/java/gdaswani/sn/client/oauth/model/Configuration.java
+++ src/main/java/gdaswani/sn/client/oauth/model/Configuration.java
@@ -11,104 +11,109 @@ import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 public final class Configuration {
 
-    private static final String CONFIG_FILE = "./conf/client.prop";
+	private static final String CONFIG_FILE = "./conf/client.prop";
 
-    private static final Logger LOGGER = Logger.getLogger(Configuration.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(Configuration.class.getName());
 
-    private String backupFolder;
-    private String loginPageURL;
-    private final Properties prop;
-    private String snClientId;
-    private String snClientSecret;
-    private String snOAuthEndPoint;
-    private String snPassword;
-    private int snRefreshLimitInSeconds;
-    private String snUsername;
-    private String updateSetListURL;
-    private String updateSetURL;
+	private String backupFolder;
+	private String loginPageURL;
+	private final Properties prop;
+	private String snBaseURL;
+	private String snClientId;
+	private String snClientSecret;
+	private String snOAuthEndPoint;
+	private String snPassword;
+	private int snRefreshLimitInSeconds;
+	private String snUsername;
+	private String updateSetListURL;
+	private String updateSetURL;
 
-    public Configuration() {
+	public Configuration() {
 
-	super();
+		super();
 
-	prop = new Properties();
+		prop = new Properties();
 
-	LOGGER.log(Level.CONFIG, "loading config file");
+		LOGGER.log(Level.CONFIG, "loading config file");
 
-	try (FileInputStream fis = new FileInputStream(new File(CONFIG_FILE))) {
+		try (FileInputStream fis = new FileInputStream(new File(CONFIG_FILE))) {
 
-	    prop.load(fis);
+			prop.load(fis);
 
-	} catch (IOException e) {
+		} catch (IOException e) {
 
-	    LOGGER.log(Level.SEVERE, e.getMessage());
+			LOGGER.log(Level.SEVERE, e.getMessage());
 
-	    throw new IllegalStateException("Could not read configuration from file " + CONFIG_FILE);
+			throw new IllegalStateException("Could not read configuration from file " + CONFIG_FILE);
+
+		}
+
+		initialize();
 
 	}
 
-	initialize();
+	public String getBackupFolder() {
+		return backupFolder;
+	}
 
-    }
+	public String getLoginPageURL() {
+		return loginPageURL;
+	}
 
-    public String getBackupFolder() {
-	return backupFolder;
-    }
+	public String getSnBaseURL() {
+		return snBaseURL;
+	}
 
-    public String getLoginPageURL() {
-	return loginPageURL;
-    }
+	public String getSnClientId() {
+		return snClientId;
+	}
 
-    public String getSnClientId() {
-	return snClientId;
-    }
+	public String getSnClientSecret() {
+		return snClientSecret;
+	}
 
-    public String getSnClientSecret() {
-	return snClientSecret;
-    }
+	public String getSnOAuthEndPoint() {
+		return snOAuthEndPoint;
+	}
 
-    public String getSnOAuthEndPoint() {
-	return snOAuthEndPoint;
-    }
+	public String getSnPassword() {
+		return snPassword;
+	}
 
-    public String getSnPassword() {
-	return snPassword;
-    }
+	public int getSnRefreshLimitInSeconds() {
+		return snRefreshLimitInSeconds;
+	}
 
-    public int getSnRefreshLimitInSeconds() {
-	return snRefreshLimitInSeconds;
-    }
+	public String getSnUsername() {
+		return snUsername;
+	}
 
-    public String getSnUsername() {
-	return snUsername;
-    }
+	public String getUpdateSetListURL() {
+		return updateSetListURL;
+	}
 
-    public String getUpdateSetListURL() {
-	return updateSetListURL;
-    }
+	public String getUpdateSetURL() {
+		return updateSetURL;
+	}
 
-    public String getUpdateSetURL() {
-	return updateSetURL;
-    }
+	private void initialize() {
 
-    private void initialize() {
+		snOAuthEndPoint = prop.getProperty("snOAuthEndPoint");
+		snClientSecret = prop.getProperty("snClientSecret");
+		snClientId = prop.getProperty("snClientId");
+		snUsername = prop.getProperty("snUsername");
+		snPassword = prop.getProperty("snPassword");
+		snRefreshLimitInSeconds = Integer.parseInt(prop.getProperty("snRefreshLimitInSeconds"));
 
-	snOAuthEndPoint = prop.getProperty("snOAuthEndPoint");
-	snClientSecret = prop.getProperty("snClientSecret");
-	snClientId = prop.getProperty("snClientId");
-	snUsername = prop.getProperty("snUsername");
-	snPassword = prop.getProperty("snPassword");
-	snRefreshLimitInSeconds = Integer.parseInt(prop.getProperty("snRefreshLimitInSeconds"));
+		updateSetListURL = prop.getProperty("updateSetListURL");
+		updateSetURL = prop.getProperty("updateSetURL");
+		loginPageURL = prop.getProperty("loginPageURL");
+		backupFolder = prop.getProperty("backupFolder");
+		snBaseURL = prop.getProperty("snBaseURL");
+	}
 
-	updateSetListURL = prop.getProperty("updateSetListURL");
-	updateSetURL = prop.getProperty("updateSetURL");
-	loginPageURL = prop.getProperty("loginPageURL");
-	backupFolder = prop.getProperty("backupFolder");
-
-    }
-
-    public String toString() {
-	return new ReflectionToStringBuilder(this).appendSuper(super.toString()).toString();
-    }
+	public String toString() {
+		return new ReflectionToStringBuilder(this).appendSuper(super.toString()).toString();
+	}
 
 }
