fd706943d29847341eba51c63593958047c4a580
gdaswani <georgedaswani@gmail.com>
2024-12-18 21:47:03 -0800

    Add files via upload

--- src/main/java/gdaswani/sn/client/tasks/PerformExport.java
+++ src/main/java/gdaswani/sn/client/tasks/PerformExport.java
@@ -36,194 +36,197 @@ import gdaswani.sn.client.oauth.sn.SNProvider;
 
 public final class PerformExport implements Runnable {
 
-    private static final Logger LOGGER = Logger.getLogger(PerformExport.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(PerformExport.class.getName());
 
-    private final Configuration config;
+	private final Configuration config;
 
-    private final SNProvider snProvider;
+	private final SNProvider snProvider;
 
-    public PerformExport() {
+	public PerformExport() {
 
-	super();
+		super();
 
-	this.config = new Configuration();
-	this.snProvider = new SNProvider(config);
+		this.config = new Configuration();
+		this.snProvider = new SNProvider(config);
 
-    }
+	}
 
-    private void exportAndSaveUpdateSets(List<UpdateSetInfo> infos) throws IOException, InterruptedException {
+	private void exportAndSaveUpdateSets(List<UpdateSetInfo> infos) throws IOException, InterruptedException {
 
-	ChromeOptions cOptions = new ChromeOptions();
+		ChromeOptions cOptions = new ChromeOptions();
 
-	cOptions.addArguments("--headless=new");
+		cOptions.addArguments("--headless=new");
+		cOptions.addArguments("--allow-running-insecure-content");
+		cOptions.addArguments("--unsafely-treat-insecure-origin-as-secure=" + config.getSnBaseURL());
 
-	HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-	chromePrefs.put("profile.default_content_settings.popups", 0);
-	chromePrefs.put("download.default_directory", config.getBackupFolder());
-	chromePrefs.put("safebrowsing.enabled", false);
+		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+		chromePrefs.put("profile.default_content_settings.popups", 0);
+		chromePrefs.put("download.default_directory", config.getBackupFolder());
+		chromePrefs.put("download.prompt_for_download", false);
+		chromePrefs.put("safebrowsing.enabled", true);
 
-	cOptions.setExperimentalOption("prefs", chromePrefs);
+		cOptions.setExperimentalOption("prefs", chromePrefs);
 
-	WebDriver webDriver = new ChromeDriver(cOptions);
+		WebDriver webDriver = new ChromeDriver(cOptions);
 
-	LOGGER.log(Level.INFO, "Set WebDriver options");
+		LOGGER.log(Level.INFO, "Set WebDriver options");
 
-	try {
+		try {
 
-	    // this one uses a browser, not REST - sadly, ServiceNow does not export the
-	    // platforms management features as APIs
+			// this one uses a browser, not REST - sadly, ServiceNow does not export the
+			// platforms management features as APIs
 
-	    webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-	    webDriver.manage().window().maximize();
+			webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+			webDriver.manage().window().maximize();
 
-	    webDriver.get(config.getLoginPageURL());
+			webDriver.get(config.getLoginPageURL());
 
-	    LOGGER.log(Level.FINEST, "webDriver = " + webDriver);
+			LOGGER.log(Level.FINEST, "webDriver = " + webDriver);
 
-	    // this is the LOGIN FORM
+			// this is the LOGIN FORM
 
-	    WebElement user = webDriver.findElement(By.name("user_name"));
-	    user.sendKeys(config.getSnUsername());
+			WebElement user = webDriver.findElement(By.name("user_name"));
+			user.sendKeys(config.getSnUsername());
 
-	    WebElement password = webDriver.findElement(By.name("user_password"));
-	    password.sendKeys(config.getSnPassword());
+			WebElement password = webDriver.findElement(By.name("user_password"));
+			password.sendKeys(config.getSnPassword());
 
-	    LOGGER.log(Level.INFO, "logging in");
+			LOGGER.log(Level.INFO, "logging in");
 
-	    webDriver.findElement(By.cssSelector("button.btn-primary")).click();
+			webDriver.findElement(By.cssSelector("button.btn-primary")).click();
 
-	    for (UpdateSetInfo info : infos) {
+			for (UpdateSetInfo info : infos) {
 
-		LOGGER.log(Level.INFO, "exporting, updateSetInfo = " + info);
+				LOGGER.log(Level.INFO, "exporting, updateSetInfo = " + info);
 
-		// head over to a LOCAL UPDATE SET RECORD
+				// head over to a LOCAL UPDATE SET RECORD
 
-		String updateSetURL = config.getUpdateSetURL().replaceAll("%SYS_ID%", info.getSysId());
+				String updateSetURL = config.getUpdateSetURL().replaceAll("%SYS_ID%", info.getSysId());
 
-		LOGGER.log(Level.FINEST, "opening updateSetURL = " + updateSetURL);
+				LOGGER.log(Level.FINEST, "opening updateSetURL = " + updateSetURL);
 
-		webDriver.get(updateSetURL);
+				webDriver.get(updateSetURL);
 
-		// wait for GSFT_MAIN IFRAME
+				// wait for GSFT_MAIN IFRAME
 
-		LOGGER.log(Level.FINEST, "waiting for gsft_main");
+				LOGGER.log(Level.FINEST, "waiting for gsft_main");
 
-		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
-		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("gsft_main"));
+				WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+				wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("gsft_main"));
 
-		// click on the "Export to XML" UI Action
+				// click on the "Export to XML" UI Action
 
-		LOGGER.log(Level.FINEST, "waiting for 'Export to XML' link");
+				LOGGER.log(Level.FINEST, "waiting for 'Export to XML' link");
 
-		wait.until(
-			ExpectedConditions.elementToBeClickable(By.cssSelector("a#fb1a56050a0a3c1e01f8b4066aff9aa7")));
+				wait.until(
+						ExpectedConditions.elementToBeClickable(By.cssSelector("a#fb1a56050a0a3c1e01f8b4066aff9aa7")));
 
-		webDriver.findElement(By.cssSelector("a#fb1a56050a0a3c1e01f8b4066aff9aa7")).click();
+				webDriver.findElement(By.cssSelector("a#fb1a56050a0a3c1e01f8b4066aff9aa7")).click();
 
-		String downloadsFolder = config.getBackupFolder();
+				String downloadsFolder = config.getBackupFolder();
 
-		// check for download completion
+				// check for download completion
 
-		LOGGER.log(Level.FINEST, "waiting for downloaded file, folder = " + downloadsFolder);
+				LOGGER.log(Level.FINEST, "waiting for downloaded file, folder = " + downloadsFolder);
 
-		boolean fileExists = false;
-		int retryCount = 0;
-		Path matchingFile = null;
+				boolean fileExists = false;
+				int retryCount = 0;
+				Path matchingFile = null;
 
-		while (!fileExists && retryCount < 10) {
+				while (!fileExists && retryCount < 10) {
 
-		    Thread.sleep(2500);
+					Thread.sleep(2500);
 
-		    Object[] matchingDownloads = Files.list(Path.of(downloadsFolder)).filter(p -> {
+					Object[] matchingDownloads = Files.list(Path.of(downloadsFolder)).filter(p -> {
 
-			String fileName = p.getFileName().toString();
+						String fileName = p.getFileName().toString();
 
-			if (fileName.startsWith("sys_remote_update_set_") && fileName.endsWith(".xml")) {
-			    return true;
-			} else {
-			    return false;
+						if (fileName.startsWith("sys_remote_update_set_") && fileName.endsWith(".xml")) {
+							return true;
+						} else {
+							return false;
+						}
+
+					}).toArray();
+
+					fileExists = matchingDownloads.length > 0;
+
+					if (fileExists) {
+						matchingFile = (Path) matchingDownloads[0];
+					} else {
+						retryCount++;
+					}
+				}
+
+				if (retryCount >= 10) {
+					throw new FileNotFoundException("Failed to download");
+				}
+
+				LOGGER.log(Level.FINEST, "fileToRename = " + matchingFile);
+
+				String filteredFNFull = downloadsFolder + info.getName().replaceAll("[\\\\/:*?\"<>|]", "") + ".xml";
+
+				Path newName = Path.of(filteredFNFull);
+
+				Files.move(matchingFile, newName, StandardCopyOption.REPLACE_EXISTING);
+
+				LOGGER.log(Level.FINEST, "fileFinalName = " + newName);
+
 			}
 
-		    }).toArray();
+			LOGGER.log(Level.INFO, "task completed");
 
-		    fileExists = matchingDownloads.length > 0;
-
-		    if (fileExists) {
-			matchingFile = (Path) matchingDownloads[0];
-		    } else {
-			retryCount++;
-		    }
+		} finally {
+			webDriver.quit();
 		}
 
-		if (retryCount >= 10) {
-		    throw new FileNotFoundException("Failed to download");
+	}
+
+	public void run() {
+
+		ObjectMapper mapper = JsonMapper.builder().enable(SerializationFeature.INDENT_OUTPUT).build();
+
+		AccessToken snAccessToken = snProvider.getAccessToken();
+
+		HttpRequest request = null;
+
+		HttpClient client = HttpClient.newHttpClient();
+
+		try {
+
+			// this calls the TABLE API to extract a list of recent update sets
+
+			URI actualEndPoint = null;
+
+			actualEndPoint = URI.create(config.getUpdateSetListURL());
+
+			request = HttpRequest.newBuilder().uri(actualEndPoint).header("Content-Type", "application/json")
+					.header("Accept", "application/json")
+					.header("Authorization", snAccessToken.generateAuthorizationValue()).GET().build();
+
+			LOGGER.log(Level.FINEST, "actualEndPoint = " + actualEndPoint);
+
+			HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+			int statusCode = response.statusCode();
+
+			LOGGER.log(Level.FINEST, "statusCode = " + Integer.toString(statusCode));
+
+			LOGGER.log(Level.FINEST, "body = " + response.body());
+
+			UpdateSetListResult container = mapper.readValue(response.body(), UpdateSetListResult.class);
+
+			LOGGER.log(Level.INFO, "retrieved UPDATE SET LIST via table API");
+
+			if (container.getResult().size() > 0) {
+				exportAndSaveUpdateSets(container.getResult());
+			}
+
+		} catch (Throwable t) {
+			LOGGER.log(Level.SEVERE, t.getMessage(), t);
+			throw new IllegalStateException(t);
 		}
 
-		LOGGER.log(Level.FINEST, "fileToRename = " + matchingFile);
-
-		String filteredFNFull = downloadsFolder + info.getName().replaceAll("[\\\\/:*?\"<>|]", "") + ".xml";
-
-		Path newName = Path.of(filteredFNFull);
-
-		Files.move(matchingFile, newName, StandardCopyOption.REPLACE_EXISTING);
-
-		LOGGER.log(Level.FINEST, "fileFinalName = " + newName);
-
-	    }
-
-	    LOGGER.log(Level.INFO, "task completed");
-
-	} finally {
-	    webDriver.quit();
 	}
-
-    }
-
-    public void run() {
-
-	ObjectMapper mapper = JsonMapper.builder().enable(SerializationFeature.INDENT_OUTPUT).build();
-
-	AccessToken snAccessToken = snProvider.getAccessToken();
-
-	HttpRequest request = null;
-
-	HttpClient client = HttpClient.newHttpClient();
-
-	try {
-
-	    // this calls the TABLE API to extract a list of recent update sets
-
-	    URI actualEndPoint = null;
-
-	    actualEndPoint = URI.create(config.getUpdateSetListURL());
-
-	    request = HttpRequest.newBuilder().uri(actualEndPoint).header("Content-Type", "application/json")
-		    .header("Accept", "application/json")
-		    .header("Authorization", snAccessToken.generateAuthorizationValue()).GET().build();
-
-	    LOGGER.log(Level.FINEST, "actualEndPoint = " + actualEndPoint);
-
-	    HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-
-	    int statusCode = response.statusCode();
-
-	    LOGGER.log(Level.FINEST, "statusCode = " + Integer.toString(statusCode));
-
-	    LOGGER.log(Level.FINEST, "body = " + response.body());
-
-	    UpdateSetListResult container = mapper.readValue(response.body(), UpdateSetListResult.class);
-
-	    LOGGER.log(Level.INFO, "retrieved UPDATE SET LIST via table API");
-
-	    if (container.getResult().size() > 0) {
-		exportAndSaveUpdateSets(container.getResult());
-	    }
-
-	} catch (Throwable t) {
-	    LOGGER.log(Level.SEVERE, t.getMessage(), t);
-	    throw new IllegalStateException(t);
-	}
-
-    }
 
 }
